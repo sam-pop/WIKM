d8d761b0b7fbc93122e4806a8bd21f474b9bcd7b
sam-pop <poplovitch.samuel@gmail.com>
2018-06-12 20:34:19 -0400

    added todo comments

--- clarifai.js
+++ clarifai.js
@@ -1,10 +1,7 @@
-const Clarifai = require('clarifai');
 require("dotenv").config();
-
+const Clarifai = require('clarifai');
 const CLARIFAI_FOOD_MODEL = "bd367be194cf45149e75f01d59f77ba7";
 const SECRET_API_KEY = process.env.key;
-
-
 
 // instantiate a new Clarifai app passing in your api key.
 const cApp = new Clarifai.App({
@@ -12,9 +9,18 @@ const cApp = new Clarifai.App({
 });
 
 
+let analyzeImage = ""; //TODO: pass image url / location
+
+
+/*
+TODO:
+1) pass the api the image url / location
+2) map out the response object into small chuncks of information
+3) filter and manipulate the information for our needs
+*/
 
 // predict the contents of an image by passing in a url
-cApp.models.predict(CLARIFAI_FOOD_MODEL, 'https://reciperhapsody.files.wordpress.com/2010/02/chicken-ranch-tacos-1-23-10_edited-1.jpg').then(
+cApp.models.predict(CLARIFAI_FOOD_MODEL, analyzeImage).then(
     function (response) {
         console.log(response); //response.outputs[0].data
     },
