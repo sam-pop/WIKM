7a5995e8f43878837e3497a8b8e0dccd18d8bf9d
sam-pop <poplovitch.samuel@gmail.com>
2018-06-13 11:45:16 -0400

    added cloudinary API image upload. fixed the clarifai api call to use the returned image url

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 require("dotenv").config();
 const Clarifai = require('clarifai');
+const cloudinary = require('cloudinary');
 const CLARIFAI_FOOD_MODEL = "bd367be194cf45149e75f01d59f77ba7";
-const SECRET_API_KEY = process.env.key;
+const CLARIFAI_API_KEY = process.env.key;
+const CLOUDINARY_API_PUBLIC = process.env.publicKey;
+const CLOUDINARY_API_SECRET = process.env.secretKey;
 
-// instantiate a new Clarifai app passing in your api key.
+let analyzeImage = ""; // will hold the image url
+
+// init Clarifai API
 const cApp = new Clarifai.App({
-    apiKey: SECRET_API_KEY
+    apiKey: CLARIFAI_API_KEY
 });
 
+// init Cloudinary API
+cloudinary.config({
+    cloud_name: 'samp',
+    api_key: CLOUDINARY_API_PUBLIC,
+    api_secret: CLOUDINARY_API_SECRET
+});
 
-let analyzeImage = ""; //TODO: pass image url / location
-
-
+// uploads the image to a remote server using the cloudinary API and returns a link to the stored image
+// using the image link - performs a model predict query to the clarifai (machine learning) API
+cloudinary.uploader.upload("./test.jpg", function (result) {
+    if (result)
+        analyzeImage = result.url;
+    console.log(result.url);
+    cApp.models.predict(CLARIFAI_FOOD_MODEL, analyzeImage).then(
+        function (response) {
+            console.log(response.outputs[0].data); //TODO: perform meaningful task with the data
+        },
+        function (err) {
+            console.error(err);
+        }
+    );
+});
 /*
 TODO:
 1) pass the api the image url / location
@@ -19,12 +42,8 @@ TODO:
 3) filter and manipulate the information for our needs
 */
 
-// predict the contents of an image by passing in a url
-cApp.models.predict(CLARIFAI_FOOD_MODEL, analyzeImage).then(
-    function (response) {
-        console.log(response); //response.outputs[0].data
-    },
-    function (err) {
-        console.error(err);
-    }
-);
+// predict the contents of an image by url
+
+
+// unsigned uploads Preset name: kd18s7co
+// uploads to https://res.cloudinary.com/samp/image/upload/
