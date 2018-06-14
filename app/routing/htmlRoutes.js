5cd8e7f554ec0eb8a3706f3369664d7c0a9720a6
sam-pop <poplovitch.samuel@gmail.com>
2018-06-14 17:44:39 -0400

    app.js deprecated, api routes & html routes updated

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,15 +3,6 @@ const path = require('path');
 
 module.exports = function (app) {
 
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-
-    //TODO: DELETE (testing) 
-    app.get('/test2', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/test-sam.html'));
-    });
-
     // default route
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
