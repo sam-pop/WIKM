e6171445137ce6dee0eda8c4f1a2cc84409d0a57
sam-pop <poplovitch.samuel@gmail.com>
2018-06-14 19:01:56 -0400

    router update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 module.exports = function (app) {
 
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
     // default route
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
