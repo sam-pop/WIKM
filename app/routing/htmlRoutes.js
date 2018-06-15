db78099d36ba27bc13edece9e25e3d8f3c98ddfc
sam-pop <poplovitch.samuel@gmail.com>
2018-06-15 10:39:02 -0400

    move index.html js and css to seperate files in the assets folder + updating routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,13 +3,20 @@ const path = require('path');
 
 module.exports = function (app) {
 
+    // public access files
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-
+    app.get('/style.css', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/assets/style.css'));
+    });
+    app.get('/index.js', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/assets/index.js'));
+    });
     app.get('/about', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/about.html'));
     });
+
 
     // default route
     app.get('*', function (req, res) {
